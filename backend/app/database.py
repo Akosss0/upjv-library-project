eb3720a7c0b5d066bd5b07053bba60a201c08f05
ArthurJacquot <arthur.jacquot@etud.u-picardie.fr>
2026-01-19 19:37:30 +0100

    Fix build error

--- backend/app/database.py
+++ backend/app/database.py
@@ -22,22 +22,6 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
-class Groupe(SQLModel, table=True):
-    id: int | None = Field(default= None, primary_key=True)
-    name: str = Field(index=True)
-
-class Emprunt(SQLModel, table=True):
-    __tablename__: str = "emprunts"
-
-    EMPRUNT_ID: int | None = Field(default=None, primary_key=True)
-    EXEMPLAIRE_ID: int = Field(foreign_key="exemplaires.EXEMPLAIRE_ID")
-    UTILISATEUR_ID: int = Field(foreign_key="utilisateurs.UTILISATEUR_ID")
-    DATE_EMPRUNT: date = Field(sa_column=Column(Date))
-    DATE_RETOUR_PREVU: date = Field(sa_column=Column(Date))
-    DATE_RETOUR_EFFECTUE: date | None = Field(default=None, sa_column=Column(Date, nullable=True))
-    STATUT_ID: int = Field(foreign_key="statuts.STATUT_ID")
-
-
 def get_db():
     db = SessionLocal()
     try:

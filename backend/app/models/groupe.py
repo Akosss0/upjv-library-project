1b9679c334536a9ba947574ad0225550642e632c
ArthurJacquot <arthur.jacquot@etud.u-picardie.fr>
2026-02-03 18:45:52 +0100

    Ajoute les groupes

--- backend/app/models/groupe.py
+++ backend/app/models/groupe.py
@@ -1,7 +1,8 @@
-from sqlmodel import SQLModel, Field, Column, Date
+from sqlmodel import SQLModel, Field, Column, String
+from app.utils import GroupeNom
 
 class Groupe(SQLModel, table=True):
     __tablename__:str = "groupes"
 
-    id: int | None = Field(default=None, primary_key=True)
-    name: str = Field()
+    GROUPE_ID: int | None = Field(default=None, primary_key=True)
+    NOM: GroupeNom = Field(sa_column=Column(String(20), nullable=False))

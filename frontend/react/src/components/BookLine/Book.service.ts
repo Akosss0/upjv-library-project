367abd74cbfb3654b0641467d89ccf52ad72e0ae
Julian <juliancazin.pro@gmail.com>
2026-02-14 21:20:34 +0100

    Fix with calling books

--- frontend/react/src/components/BookLine/Book.service.ts
+++ frontend/react/src/components/BookLine/Book.service.ts
@@ -1,9 +1,10 @@
 import { API_URL } from "../../App";
+import type { BookProps } from "./BookLine";
 
 
 export const BookService = {
 
-    async getBooks() {
+    async getBooks(): Promise<BookProps[]> {
         try {
             const response = await fetch(API_URL);
             if (!response.ok) throw new Error('Erreur réseau');
@@ -11,6 +12,27 @@ export const BookService = {
         } catch (error) {
             console.error("Erreur lors de la récupération des livres:", error);
             return [];
+        }
+    },
+
+    async postBook(book: Omit<BookProps, 'livre_id'>): Promise<BookProps | null> {
+        try {
+            const response = await fetch(API_URL, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(book)
+            });
+            
+            if (!response.ok) {
+                throw new Error(`Erreur HTTP: ${response.status}`);
+            }
+            
+            return await response.json();
+        } catch (error) {
+            console.error("Erreur lors de la création du livre:", error);
+            return null;
         }
     },
 
